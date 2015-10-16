7250b6afd9f25be555383ec4ce3b9b98c644ff0e
Al Krinker <al.krinker@gmail.com>
2015-10-16 15:06:16 -0400

    Clean up

--- RecommendationSystem/Test/test_recommender_via_minkowski.py
+++ RecommendationSystem/Test/test_recommender_via_minkowski.py
@@ -14,10 +14,11 @@ class TestRecommenderViaMinkowski(unittest.TestCase):
     def setUp(self):
         self.ratings = user_music_ratings.ratings
 
-    def test_compute_pearson_correlation_coefficient_Angelica_Bill(self):
+    def test_recommender_via_minkowski(self):
         user_in_question = "Sam"
         results = recommender_via_minkowski.recommend(user_in_question, ratings)
         print "List of movies that %s might like based on most similar user %s taste: %r" \
             % (user_in_question, results[0], results[1])
         self.assertEqual('Chan', results[0])
-        self.assertEqual(('Deadmau5', 1.0), results[1][0])
+        self.assertEqual('Deadmau5', results[1][0][0])
+        self.assertEqual(1.0, results[1][0][1])
